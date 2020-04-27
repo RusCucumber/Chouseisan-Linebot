53d70ccac3c38ae00209c69cf0e82b7c6a7a5cd2
Ryuki <w.d.ryu.m@gmail.com>
2020-04-27 23:12:42 +0900

    first ver

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ from linebot.models import (
 import re
 import datetime
 
+import requests
+import time
+from bs4 import BeautifulSoup
+
 # __name__をFlaskのアプリとして利用
 app = Flask(__name__)
 
@@ -49,7 +53,6 @@ def callback():
     return 'OK'
 
 def check_message(message):
-
     schedule = ""
     pattern_date = '^\d\d?/\d\d?$'
 
@@ -58,22 +61,70 @@ def check_message(message):
         if result:
             try:
                 date_formatted = datetime.datetime.strptime(date, "%m/%d")
-                schedule += (date + '\n')
+                schedule += (date + "\n")
             except:
-                return "Error"
-
+                return "-1"
         else:
-            return "Error"
-
+            return "-1"
     return schedule
+
+def get_chouseisan(date):
+    try:
+        top_page = requests.get('https://chouseisan.com/schedule/newEvent/create')
+        soup_top_page = BeautifulSoup(top_page.text, 'html.parser')
+        chousei_token_input_tag = soup_top_page.find_all('input', id='chousei_token')
+        chousei_token = chousei_token_input_tag[0].get('value')
+    except:
+        return "-1"
+    time.sleep(3)
+
+    session = requests.session()
+    response = session.get('https://chouseisan.com/schedule/newEvent/create')
+
+    event = {
+        'name': 'schedule',
+        'kouho': date,
+        'chousei_token': chousei_token
+    }
+
+    try:
+        create_page = session.post('https://chouseisan.com/schedule/newEvent/create', data=event)
+    except:
+        return "-1"
+    time.sleep(3)
+
+    try:
+        soup_create_page = BeautifulSoup(create_page.text, 'html.parser')
+        input_tag_url = soup_create_page.find_all('input', class_='form-input new-event-url-input')
+        schedule_url = input_tag_url[0].get('value')
+        return schedule_url
+    except:
+        return "-1"
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    hoge = check_message(event.message.text)
+    response = ""
+
+    ## メッセージが仕様通りかチェック
+    date = check_message(event.message.text)
+    if (date == "-1"):
+        response = """Error
+        argument should be like this:
+            month/day
+        for example:
+            12/1"""
+    
+    chouseisan = get_chouseisan(date)
+    if (chouseisan == "-1"):
+        response = """Error
+        Failed to get URL of chousei-san."""
+    else:
+        response = chouseisan
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=hoge))
+        TextSendMessage(text=response))
 
 if __name__ == "__main__":
 #    app.run()
